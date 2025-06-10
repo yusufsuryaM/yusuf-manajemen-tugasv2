c81345c9923b1e7fc3a92ceb5235ed20ca6b4388
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-06-11 00:58:56 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/com/yusuf0080/manajementugasassessment3/model/Hewan.kt
@@ -0,0 +1,7 @@
+package com.yusuf0080.manajementugasassessment3.model
+
+data class Hewan(
+    val nama: String,
+    val namaLatin: String,
+    val imageId: String
+)
--- app/src/main/java/com/yusuf0080/manajementugasassessment3/network/HewanApiService.kt
+++ app/src/main/java/com/yusuf0080/manajementugasassessment3/network/HewanApiService.kt
@@ -1,19 +1,26 @@
 package com.yusuf0080.manajementugasassessment3.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.yusuf0080.manajementugasassessment3.model.Hewan
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://raw.githubusercontent.com/indraazimi/mobpro1-compose/static-api/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface HewanApiService {
     @GET("static-api.json")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 
 object HewanApi {
