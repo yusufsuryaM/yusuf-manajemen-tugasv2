49236f6d66794595c4a7ea9dee8da567f5e8154a
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-06-10 19:22:05 +0700

    Initial commit

---
+++ app/src/main/java/com/yusuf0080/manajementugasassessment3/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.yusuf0080.manajementugasassessment3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yusuf0080.manajementugasassessment3.ui.theme.ManajemenTugasAssessment3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ManajemenTugasAssessment3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ManajemenTugasAssessment3Theme {
+        Greeting("Android")
+    }
+}
