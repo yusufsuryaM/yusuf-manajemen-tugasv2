596258562ac84a1d0e1aaa1e1616f5f8f446631b
yusufsuryaM <yusufsuryamulyawan@gmail.com>
2025-06-10 19:30:11 +0700

    Initial commit

---
+++ app/src/main/java/com/yusuf0080/manajementugasassessment3/ui/screen/MainScreen.kt
@@ -0,0 +1,61 @@
+package com.yusuf0080.manajementugasassessment3.ui.screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.yusuf0080.manajementugasassessment3.R
+import com.yusuf0080.manajementugasassessment3.ui.theme.ManajemenTugasAssessment3Theme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ){ innerPadding ->
+        ScreenContent((Modifier.padding(innerPadding)))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier){
+    Text(
+        text = "Hello Android",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    ManajemenTugasAssessment3Theme {
+        MainScreen()
+    }
+}
